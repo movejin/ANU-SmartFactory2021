12e941a5d80fdc4037efec8ddd640fafd5100708
movejin <t_o159@naver.com>
2021-09-04 11:20:53 +0900

    파일 수정

--- src/machine/trctmethod.py
+++ src/machine/trctmethod.py
@@ -10,29 +10,5 @@ def read_sensor() :
     digit_val=GPIO.input(DIGIT)
     return digit_val
 
-"""
-def trct_main():
-    
-    while True:
-        
-        num2 = 0
-        num2 = input()
-        
-        if num2=='1':
-            digit_val=GPIO.input(DIGIT)
-            print("dlgital : %d " %(digit_val))
-            time.sleep(0.5)
-            
-        elif num2=='2':
-            break
-        
 
-try:
-    print("센서 함수를 호출했습니다.")
-    print("사용하시겠습니까: 1 \n다른 센서 사용: 2")
-    trct_main()
-
-finally:
-    GPIO.cleanup()
-"""
     
